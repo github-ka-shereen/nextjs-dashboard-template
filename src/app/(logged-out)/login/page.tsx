935838441cb01182f90c87442605a06d5d019478
Shereni <info@aewebsites.co.zw>
2024-05-01 17:21:21 +0200

    Feature: Signup page half done

--- src/app/(logged-out)/login/page.tsx
+++ src/app/(logged-out)/login/page.tsx
@@ -1,5 +1,66 @@
+'use client';
 
+import * as React from "react"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import Link from 'next/link';
+import { PersonStandingIcon } from "lucide-react";
 
 export default function LogInPage() {
-  return <div>Login</div>;
+  return (
+    <>
+    <PersonStandingIcon size={50}/>
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Log In</CardTitle>
+        <CardDescription>Log In to your Admin Account.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Next.js</SelectItem>
+                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                  <SelectItem value="astro">Astro</SelectItem>
+                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between items-center">
+        <small>Do You Have An Account?</small>
+        <Button asChild variant="outline"><Link href='/sign-up'>Sign Up</Link></Button>
+      </CardFooter>
+    </Card>
+    </>
+  );
 }
